cd42360dbdf55fbd44445be9bdc791d1f156439c
jonathan <pyjonathan31@gmail.com>
2019-03-22 15:45:52 +0100

    init

---
+++ projetTechniqueBack/Controllers/UserController.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using projetTechniqueBack.Models;
+
+namespace projetTechniqueBack.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserContext _context;
+
+        //creation d'un user au démarrage de l'api
+        public UserController(UserContext context)
+        {
+            _context = context;
+
+            if (_context.AuthentificationItems.Count() == 0)
+            {
+                _context.AuthentificationItems.Add(new UserItem { Id = "toto", Mdp="toto" });
+                _context.SaveChanges();
+            }
+        }
+
+        // POST: api/Authentification/Login
+        [HttpPost("Login")]
+        public async Task<ActionResult<UserItem>> PostLogin(UserItem user)
+        {
+            var AuthentificationItem = await _context.AuthentificationItems.FindAsync(user.Id);
+
+            if (AuthentificationItem == null)
+            {
+                return NotFound();
+            } else
+            {
+                if (AuthentificationItem.Mdp.Equals(user.Mdp))
+                {
+                    return AuthentificationItem;
+                }
+                else
+                {
+                    return BadRequest();
+                }  
+            }
+        }
+
+        //TODO : A supprimer
+        // GET: api/Authentification
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UserItem>>> GetAuthentificationItems()
+        {
+            return await _context.AuthentificationItems.ToListAsync();
+        }
+
+        // POST: api/Authentification/Register
+        [HttpPost("Register")]
+        public async Task<ActionResult<UserItem>> PostAuthentificationItem(UserItem item)
+        {
+            var AuthentificationItem = await _context.AuthentificationItems.FindAsync(item.Id);
+
+            if (AuthentificationItem == null)
+            {
+                _context.AuthentificationItems.Add(item);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        // POST: api/Authentification/Modification
+        [HttpPost("Modification")]
+        public async Task<ActionResult<UserItem>> PostModifUserItem(UserItem item)
+        {
+            var AuthentificationItem = await _context.AuthentificationItems.FindAsync(item.Id);
+
+            if (AuthentificationItem == null)
+            {
+                _context.AuthentificationItems.Add(item);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        //TODO: vérification si personne a le droit 
+        // GET: api/Authentification/id
+        [HttpPost("{id}")]
+        public async Task<ActionResult<UserItem>> getUserItem(string id)
+        {
+            var UserItem = await _context.AuthentificationItems.FindAsync(id);
+
+            if (UserItem == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                return UserItem;
+            }
+        }
+    }
+}
---
+++ projetTechniqueBack/Models/UserItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace projetTechniqueBack.Models
+{
+    public class UserItem
+    {
+        public string Id { get; set; }
+        public string Mdp { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public int Age { get; set; }
+        public decimal Poids { get; set; }
+        public decimal Taille { get; set; }
+        public bool IsCoach { get; set; }
+    }
+}
